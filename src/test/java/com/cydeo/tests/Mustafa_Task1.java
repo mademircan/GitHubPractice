17fc393fc97bc0f8d553ce3b299a49a347463d08
mustafaornek32 <99423578+mustafaornek32@users.noreply.github.com>
2022-06-16 19:17:43 +0100

    Mustafa Task1_Started&Finished

--- src/test/java/com/cydeo/tests/Mustafa_Task1.java
+++ src/test/java/com/cydeo/tests/Mustafa_Task1.java
@@ -35,4 +35,14 @@ public class Mustafa_Task1 {
 
 
         }
+
+
 }
+/*
+TC #1: Etsy Title Verification
+1. Open Chrome browser
+2. Go to https://www.etsy.com
+3. Search for “wooden spoon”
+4. Verify title:
+Expected: “Wooden spoon | Etsy
+ */
