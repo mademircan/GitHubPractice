651c7ed107abb7e1502f809ec740501a813dd9e7
Fatma Guleryuz <fglryz.111@gmail.com>
2022-06-16 18:58:05 +0100

    task1

---
+++ src/test/java/com/cydeo/tests/Task1.java
@@ -0,0 +1,22 @@
+package com.cydeo.tests;
+
+import org.testng.annotations.Test;
+
+import java.sql.Driver;
+
+public class Task1 {
+    @Test
+    public void OpenBrowser(){
+
+        
+    }
+
+}
+/*
+TC #1: Etsy Title Verification
+1. Open Chrome browser
+2. Go to https://www.etsy.com
+3. Search for “wooden spoon”
+4. Verify title:
+Expected: “Wooden spoon | Etsy”
+ */
