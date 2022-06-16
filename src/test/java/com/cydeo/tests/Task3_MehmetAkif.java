d76e8196fa20aec7c9627d8d48218e6d5f985389
Mehmet Akif Demircan <mademircan1014@gmail.com>
2022-06-16 19:02:09 +0100

    This is my second commit

---
+++ src/test/java/com/cydeo/tests/Task3_MehmetAkif.java
@@ -0,0 +1,51 @@
+package com.cydeo.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class Task3_MehmetAkif {
+
+    @Test
+    public void dynamicPage1Test(){
+
+
+
+
+
+
+    }
+}
+
+
+
+/*
+@Test
+    public void dynamic_load_1_test(){
+        //1. Go to https://practice.cydeo.com/dynamic_loading/1
+        Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/1");
+
+        DynamicLoad1Page dynamicLoad1Page = new DynamicLoad1Page();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+
+        //2. Click to start
+        dynamicLoad1Page.startButton.click();
+
+        //3. Wait until loading bar disappears
+        wait.until(ExpectedConditions.invisibilityOf(dynamicLoad1Page.loadingBar));
+
+        //4. Assert username inputbox is displayed
+        Assert.assertTrue(dynamicLoad1Page.inputUsername.isDisplayed());
+
+        //5. Enter username: tomsmith
+        dynamicLoad1Page.inputUsername.sendKeys("tomsmith");
+
+        //6. Enter password: incorrectpassword
+        dynamicLoad1Page.inputPassword.sendKeys("somethingwrong");
+
+        //7. Click to Submit button
+        dynamicLoad1Page.submitButton.click();
+
+        //8. Assert “Your password is invalid!” text is displayed.
+        Assert.assertTrue(dynamicLoad1Page.errorMessage.isDisplayed());
+    }
+ */
