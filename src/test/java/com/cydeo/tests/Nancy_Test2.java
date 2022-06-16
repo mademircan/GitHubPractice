21ad4a0d0c5ab6840726ab8d965d3a311863b0b9
Nancy <ncyarslan@gmail.com>
2022-06-16 19:19:32 +0100

    Task 2 solution revision

--- src/test/java/com/cydeo/tests/Nancy_Test2.java
+++ src/test/java/com/cydeo/tests/Nancy_Test2.java
@@ -1,94 +1,61 @@
 package com.cydeo.tests;
 
-import com.cydeo.utilities.Driver;
-import com.github.javafaker.Faker;
-import org.openqa.selenium.Alert;
+import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class Nancy_Test2 {
 
-    public static void staticWait(int seconds){
-        try{
-            Thread.sleep(seconds*1000);
-        }catch (Exception e){
+    public static void main(String[] args) throws InterruptedException {
 
-        }
+        //Practice: Checkboxes
+        //1. Go to http://practice.cydeo.com/checkboxes
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/checkboxes");
+
+        //We have to locate checkboxes
+        WebElement checkbox1 = driver.findElement(By.xpath("//input[@name='checkbox1']"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//input[@name='checkbox2']"));
+
+        //2. Confirm checkbox #1 is NOT selected by default.
+        System.out.println("checkbox1.isSelected(), expecting false = " + checkbox1.isSelected());
+
+        //3. Confirm checkbox #2 is SELECTED by default.
+        System.out.println("checkbox2.isSelected(), expecting true = " + checkbox2.isSelected());
+
+        //4. Click checkbox #1 to select it.
+        Thread.sleep(2000);
+        checkbox1.click();
+
+        //5. Click checkbox #2 to deselect it.
+        Thread.sleep(2000);
+        checkbox2.click();
+
+        //6. Confirm checkbox #1 is SELECTED.
+        System.out.println("checkbox1.isSelected(), expecting true = " + checkbox1.isSelected());
+
+        //7. Confirm checkbox #2 is NOT selected.
+        System.out.println("checkbox2.isSelected(), expecting false = " + checkbox2.isSelected());
+
+//        if(checkbox1.isSelected()){
+//            System.out.println("verification passed");
+//        }else{
+//            System.out.println("verification fail");
+//        }
+//        //   7. Confirm checkbox #2 is NOT selected.
+//
+//        if(!checkbox2.isSelected()){
+//            System.out.println("verification passed");
+//        }else{
+//            System.out.println("verification fail");
+//        }
+
+
+
     }
-
-    // will be used in demoblaze application, to navigate to different links at the page
-    // will click on different elements/links at the page
-    public static void getLink(WebDriver driver, String link){
-        driver.findElement(By.partialLinkText(link)).click();
-        staticWait(1);
-    }
-    public static void getLink(String link){
-        Driver.getDriver().findElement(By.partialLinkText(link)).click();
-        staticWait(1);
-    }
-
-    public static double addProduct(WebDriver driver,String category, String product){
-        getLink(driver,category); // click on category
-        getLink(driver,product); // click on product
-// Let's get product's price
-        String priceText = driver.findElement(By.tagName("h3")).getText();
-        double price = Double.parseDouble(priceText.substring(1,4));
-
-        getLink(driver,"Add to cart");
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
-        return price;
-    }
-    public static double addProduct(String category, String product){
-        getLink(category); // click on category
-        getLink(product); // click on product
-// Let's get product's price
-        String priceText = Driver.getDriver().findElement(By.tagName("h3")).getText();
-        double price = Double.parseDouble(priceText.substring(1,4));
-
-        getLink("Add to cart");
-        Alert alert = Driver.getDriver().switchTo().alert();
-        alert.accept();
-        return price;
-    }
-
-    public static void fillForm(WebDriver driver){
-
-        Faker faker = new Faker();
-        driver.findElement(By.id("name")).sendKeys(faker.name().fullName());
-        driver.findElement(By.id("country")).sendKeys(faker.country().name());
-        driver.findElement(By.id("city")).sendKeys(faker.address().city());
-        driver.findElement(By.id("card")).sendKeys(faker.finance().creditCard());
-        driver.findElement(By.id("month")).sendKeys(""+faker.number().numberBetween(1,12));
-        driver.findElement(By.id("year")).sendKeys(""+faker.number().numberBetween(2022,2032));
-        driver.findElement(By.xpath("//button[.='Purchase']")).click();
-        staticWait(2);
-    }
-    public static void fillForm(){
-
-        Faker faker = new Faker();
-        Driver.getDriver().findElement(By.id("name")).sendKeys(faker.name().fullName());
-        Driver.getDriver().findElement(By.id("country")).sendKeys(faker.country().name());
-        Driver.getDriver().findElement(By.id("city")).sendKeys(faker.address().city());
-        Driver.getDriver().findElement(By.id("card")).sendKeys(faker.finance().creditCard());
-        Driver.getDriver().findElement(By.id("month")).sendKeys(""+faker.number().numberBetween(1,12));
-        Driver.getDriver().findElement(By.id("year")).sendKeys(""+faker.number().numberBetween(2022,2032));
-        Driver.getDriver().findElement(By.xpath("//button[.='Purchase']")).click();
-        staticWait(2);
-    }
-
-
-    public static void windowHandle(WebDriver driver, String pickDateWindowTitle) {
-        Set<String> windowHandles = driver.getWindowHandles();
-        for (String each : windowHandles) {
-            driver.switchTo().window(each);
-
-            if (driver.getTitle().equals(pickDateWindowTitle)) {
-                break;
-            }
-        }
-    }
-
 }
